1aa79df0bfc840b2d3e4c77817fb81e67c8d4b14
RayanIslam30 <rayislam2007@gmail.com>
2023-03-08 17:49:02 -0500

    updated registration page

--- confirm_registration.php
+++ confirm_registration.php
@@ -1,5 +1,13 @@
+<!DOCTYPE html>
 <html>
 <body>
+
+<link href="../SmallTownSteam/css/bootstrap.css" rel="stylesheet">
+<div class="container py-4">
+    <header class="pb-3 mb-4 border-bottom">
+    <div id="header">
+	</div>
+    </header>
 <?php  
 ?>
 
@@ -13,6 +21,7 @@ $date = $_POST["date"];
 $attendee_detail = "$name===$email \n";
 $filename = "$track-$date.txt";
 
+
 $filename = "classes_and_registration/$track-$date.txt";
 echo "Thank you $name for registering for the $course on $date. You will receive a confirmation email at $email";
 
@@ -24,7 +33,16 @@ fwrite($channel, $attendee_detail);
 fclose($channel);
 
 ?>
-
+<div id="footer">
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="js/scripts.js"></script>
+	<script src="//code.jquery.com/jquery.min.js"></script>
+	<script>$(function(){
+  		$("#header").load("navBar.html");
+                $("#footer").load("footer.html");
+		}); 
+	</script>
 
 
 
