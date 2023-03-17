ab9dbff16e5c0eeb2b1bd99831ebd588a883c4d2
kislam-gmail <92175102+kislam-gmail@users.noreply.github.com>
2023-03-16 21:41:23 -0400

    admin page editing

--- add-class.php
+++ add-class.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html>
-
 <head>
 <title>Add Class</title>
 <link rel="icon" type="image/x-icon" href="icon.png">
@@ -17,16 +16,31 @@
 $track = $_POST["track"];
 $date = $_POST["date"];
 $title = $_POST["title"]; 
-$new_course = "$track===$date===$title \n"
 ?>
-This is the Course that will be added
-<p id="newcourse"><?php echo $track?>===<?php echo $date?>===<?php echo $title?></p>
+ <div class="p-5 mb-4 text-bg-light rounded-3">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold"><p style="color:white">Course to Be Added</h1></p>
+        <p style="font-size:40px"><p style="color:white"> <?php echo "A $track course, on $date, titled $title." ?></p></p>
+		<form action="classes_and_registration/upcoming-classes.php" method="post">
+	    <input type="hidden" name="track" value='<?php echo "$track";?>'>
+    <input type="hidden" name="title" value='<?php echo "$title";?>'>
+    <input type="hidden" name="date" value='<?php echo "$date";?>'>
+    <input type="submit" name="Confirm New Class">
+</form>
+
+      </div>
+    </div>
+<p id="newcourse"><?php echo $track?>===<?php echo $date?>===<?php echo $title?></p> 
 <script>
 let str = document.getElementById("newcourse").innerHTML; 
 let res = str.replace(/-/g, "===");
 document.getElementById("newcourse").innerHTML = res;
 </script>
 
+<?php
+$newcourse = $_POST["newcourse"];
+echo $newcourse;
+?>
 <div id="footer">
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
